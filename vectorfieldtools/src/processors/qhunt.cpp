f4a262b631391f59d5991bf4f8e18970105cfebb
Toby <31617172+tobzed@users.noreply.github.com>
2022-02-08 14:20:20 +0100

    Update qhunt.cpp

--- vectorfieldtools/src/processors/qhunt.cpp
+++ vectorfieldtools/src/processors/qhunt.cpp
@@ -33,21 +33,20 @@ namespace inviwo {
 
 // The Class Identifier has to be globally unique. Use a reverse DNS naming scheme
 const ProcessorInfo QHunt::processorInfo_{
-    "org.inviwo.QHunt",      // Class identifier
-    "QHunt",                // Display name
-    "Undefined",              // Category
-    CodeState::Experimental,  // Code state
-    Tags::None,               // Tags
+	"org.inviwo.QHunt",	// Class identifier
+	"QHunt",		// Display name
+	"Undefined",		// Category
+	CodeState::Experimental,	// Code state
+	Tags::None,		// Tags
 };
 const ProcessorInfo QHunt::getProcessorInfo() const { return processorInfo_; }
 
 QHunt::QHunt()
-    : Processor()
-    , volume_in_("Vector_field_volume_inport")
+	: Processor()
+	, volume_in_("Vector_field_volume_inport")
 	, volume_out_("Scalar_field_volume_outport") {
-
-    addPort(volume_in_);
-	addPort(volume_out_);
+		addPort(volume_in_);
+		addPort(volume_out_);
 }
 
 void QHunt::process() {
@@ -55,7 +54,7 @@ void QHunt::process() {
 	const size3_t dims = vector_field->getDimensions();
 	// make dest volume
 	auto QHunt_vol_repr = std::make_shared<VolumeRAMPrecision<float>>(dims);
-    float* QHunt_raw_ptr = QHunt_vol_repr->getDataTyped();
+    	float* QHunt_raw_ptr = QHunt_vol_repr->getDataTyped();
 	// iterate over vector field and compute OW
 	size_t dst_index = 0;
 	float max_val = std::numeric_limits<float>::min();
@@ -65,6 +64,7 @@ void QHunt::process() {
 			for(size_t ix = 0; ix < dims.x; ++ix) {
 				// compute Omega^2 + S^2
 				const mat3 Omega2S2 = omega2s2_.get(vector_field, size3_t(ix,iy,iz));
+				// q value
 				const float res = -Omega2S2[0][0] - Omega2S2[1][1] - Omega2S2[2][2];
 				QHunt_raw_ptr[dst_index++] = res;
 				if(res > max_val) max_val = res;
@@ -73,13 +73,14 @@ void QHunt::process() {
 		}	
 	}
 	std::shared_ptr<Volume> QHunt = std::make_shared<Volume>(QHunt_vol_repr);
-    QHunt->setBasis(vector_field->getBasis());
+    	QHunt->setBasis(vector_field->getBasis());
 	QHunt->setOffset(vector_field->getOffset());
 	QHunt->copyMetaDataFrom(*vector_field);	
 	QHunt->dataMap_.dataRange = QHunt->dataMap_.valueRange = vec2(min_val, max_val);	
 	QHunt->setModelMatrix(vector_field->getModelMatrix());
 	QHunt->setWorldMatrix(vector_field->getWorldMatrix());
-    volume_out_.setData(QHunt);
+    
+	volume_out_.setData(QHunt);
 }
 
 }  // namespace inviwo
